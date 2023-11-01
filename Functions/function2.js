1a4430b4579b66592ebc958892ae6f9e52bd0747
rezina-git <budhathokirezina59@gmail.com>
2023-11-01 20:56:06 +0545

    Function exp and declare

---
+++ Functions/function2.js
@@ -0,0 +1,12 @@
+// Difference Between function declaration and function expressions
+
+//Examaple of function Declaration
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(2,3));
+//Example of function Expessions
+let sum = function (a,b){
+    return a+b;
+}
+console.log(sum (3,5));
