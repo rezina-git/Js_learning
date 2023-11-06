8ca05ac78c06f6f035da26910f5ce9e712a5e4ed
rezina-git <budhathokirezina59@gmail.com>
2023-11-06 21:37:02 +0545

    methods

--- Functions/function2.js
+++ Functions/function2.js
@@ -10,3 +10,4 @@ let sum = function (a,b){
     return a+b;
 }
 console.log(sum (3,5));
+//
--- index.js
+++ index.js
@@ -171,3 +171,8 @@ console log that total.*/
 // console.log(Name);
 // sayName();
 /////////////Methods/////////////////////////////
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
