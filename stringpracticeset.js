3572dfabed8806e7cd431f758cb04ec0ccee4d27
rezina-git <budhathokirezina59@gmail.com>
2023-11-08 16:03:45 +0545

    Examples

---
+++ stringpracticeset.js
@@ -0,0 +1,18 @@
+//Templete literals
+/*Template literals use backtick instead of quotes to define string*/
+let name = `Harry`
+let a = `This is  ${name}`
+console.log(a);
+let boy1 = "Pramod"
+let boy2 = "Shiva"
+//To print pramod is friend of shiva 
+let sentence = `${boy1} is friend of ${boy2}`
+console.log(sentence)
+//To print Adam D'Angelo
+let you = `Adam D'Angelo`
+console.log(you)
+// Use for loop to print the string
+for(i=0;i<fr.length;i++){
+    //or console.log(fr)
+    console.log(fr[0] + fr[1] + fr[2] + fr[3] + fr[4] + fr[5] + fr[6])
+}
