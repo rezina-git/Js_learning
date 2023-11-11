d6dd802a618729024006b949b895d839be7495f4
rezina-git <budhathokirezina59@gmail.com>
2023-11-11 18:49:55 +0545

    string_practice

---
+++ stringpracticeset.js
@@ -0,0 +1,13 @@
+//Templete literals
+/*Template literals use backtick instead of quotes to define string*/
+let name = `Harry`
+let a = `This is  ${name}`
+console.log(a);
+let boy1 = "Pramod"
+let boy2 = "Shiva"
+//To print pramod is friend of shiva 
+let sentence = `${boy1} is friend of ${boy2}`
+console.log(sentence)
+//To print Adam D'Angelo
+let you = `Adam D'Angelo`
+console.log(you)
