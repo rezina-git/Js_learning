f25814d138a8809c8c3b8b4ccb624ac0a3faf5f2
rezina-git <budhathokirezina59@gmail.com>
2023-11-18 16:14:22 +0545

    inbuilt_functions_inArrays

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -4,6 +4,23 @@ let studentlist = ["Tom", "Tyson"]
 //to add new data
 studentlist[2]="Harry"
 //OR
-let studentlist = new Array["Ram","Shayam","Harry"]
+let studentname = new Array("Ram","Shayam","Harry")
 //Since JS is dynamic type programming language we can use any any typle of value inside array
-let studentlist = ["Tom", "Tyson",1,true,1n]
+let studentoldlist = ["Tom", "Tyson",1,true,1n]
+
+//Builtins Arrays functions in JS:
+let studentnewlist = ["Tom", "Tyson"]
+studentnewlist.push("Thomas","Hari")
+studentnewlist.pop();
+delete studentnewlist[1];
+studentnewlist.unshift("Ram")
+ console.log(studentlist);
+ console.log(studentoldlist);
+ console.log(studentnewlist);
+ console.log(studentnewlist.join(" ,"));
+ let newDatalist = studentname.concat(studentnewlist);
+ console.log(newDatalist);
+ //slice():Returns the portion of array.
+ console.log(studentname.slice(1));
+ console.log(studentname.slice(-2));
+console.log(studentname.fill("Riya", 2))
