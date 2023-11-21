9480a47e9693c8aef346d2ce9972ade37e37c0ab
rezina-git <budhathokirezina59@gmail.com>
2023-11-21 08:45:49 +0545

    loopsInJs

---
+++ Arrays/loops.js
@@ -0,0 +1,23 @@
+//The various loops ib Js are for loop ,while loop ,do-while loop,for each loop ,for of, for in
+//for loop
+for (i=0;i<=3;i++){
+    console.log("Cat");
+}
+// while loop
+let x =0;
+while(x<3){
+    console.log("Car");
+    x++;
+}
+// do while
+let y=0;
+do{
+    console.log("Dog")
+    y++;
+}
+while(y<=4);
+// To loop array
+const cities =["Tokyo","london","Norway"]
+for(let i=0;i<cities.length;i++){
+    console.log(cities[i]);
+}
