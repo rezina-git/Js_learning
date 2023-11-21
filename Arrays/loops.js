4512e6e9d57aada6cd8d8712c1960d54f1800880
rezina-git <budhathokirezina59@gmail.com>
2023-11-21 09:13:39 +0545

    variation in loops

--- Arrays/loops.js
+++ Arrays/loops.js
@@ -21,3 +21,46 @@ const cities =["Tokyo","london","Norway"]
 for(let i=0;i<cities.length;i++){
     console.log(cities[i]);
 }
+// For loop variation in Js 
+// 1.TO work easily with array we use for each loop
+cities.forEach(function (v,i){
+    console.log(v)
+});
+
+//2. for of loop -->Used in array or string
+/*syntax:
+for(elements of iterable){
+    //body of for...of
+}
+The for of loop in js allows you to iterate over iterable objects (arrays, sets , maps , strings,etc)
+Here:
+iterable - an iterable object 
+Elements - items in iterable
+*/
+const studentlist = ["Samay","Rakesh"]
+for (let student of studentlist){
+    console.log(student);
+}
+// for of here gives values in arrays
+
+let channel= "Discord"
+for (let c of channel){
+    console.log(c)
+}
+//3. for in loop -> in gives index value in Array and key value in object
+/* syntax: for (variable in object){
+    code to be executed for each property of object
+}*/
+for ( let city in cities ){
+    console.log(city);
+}
+// o/p : 0 1 2
+
+const stds ={
+    name :"ABC",
+    address : "ktm"
+};
+for ( let s in stds){
+    console.log(s);
+}
+//o/p: keys in object
